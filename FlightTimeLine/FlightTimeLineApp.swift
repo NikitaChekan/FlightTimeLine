4df3fc276fa6a021761af24c0178ea8be1ea3fa4
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2022-12-18 21:40:52 +0300

    Initial Commit

---
+++ FlightTimeLine/FlightTimeLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTimeLineApp.swift
+//  FlightTimeLine
+//
+//  Created by jopootrivatel on 18.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlightTimeLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
